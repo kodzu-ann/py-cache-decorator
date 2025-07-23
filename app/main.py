0fc3e0d93806295d6b6ab6ffc7bdb55959191b1e
Anna S <my@example.com>
2025-07-22 23:34:56 -0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args: Any, **kwargs) -> Callable:
+        key = args
+        if key not in cache_dict:
+            print("Calculating new result")
+            value = func(*args, **kwargs)
+            cache_dict[key] = value
+            result = value
+        else:
+            print("Getting from cache")
+            result = cache_dict[key]
+        return result
+    return inner
+    return cache
